0dcbba556a601f1f82ffe92aa9f4fce1ba021a5e
Daniel <danchan020@gmail.com>
2023-01-13 16:52:36 -0800

    keyboard

--- components/Login.js
+++ components/Login.js
@@ -1,40 +1,50 @@
 import { Formik } from "formik";
-import { View, Text, TextInput, TouchableWithoutFeedback } from "react-native";
+import {
+  Keyboard,
+  View,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+} from "react-native";
 import { globalStyles } from "../styles/global";
 
 export default function Login() {
   // const handleLogin = () => {}
 
   return (
-    <View style={globalStyles.container}>
-      <Formik
-        initialValues={{ email: "", password: "" }}
-        // onSubmit={handleLogin}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-          /* and other goodies */
-        }) => (
-          <View>
-            <Text>Email</Text>
-            <TextInput
-              onChangeText={handleChange("email")}
-              value={values.email}
-            />
-            <Text>Password</Text>
-            <TextInput
-              onChangeText={handleChange("password")}
-              value={values.password}
-            />
-          </View>
-        )}
-      </Formik>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={globalStyles.container}>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          // onSubmit={handleLogin}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            /* and other goodies */
+          }) => (
+            <View>
+              <Text>Email</Text>
+              <TextInput
+                onChangeText={handleChange("email")}
+                onBlur={handleBlur("email")}
+                value={values.email}
+              />
+              <Text>Password</Text>
+              <TextInput
+                onChangeText={handleChange("password")}
+                onBlur={handleBlur("password")}
+                value={values.password}
+              />
+            </View>
+          )}
+        </Formik>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
--- components/Signup.js
+++ components/Signup.js
@@ -1,55 +1,67 @@
 import { Formik } from "formik";
-import { View, Text, TextInput, TouchableWithoutFeedback } from "react-native";
+import {
+  Keyboard,
+  View,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+} from "react-native";
 import { globalStyles } from "../styles/global";
 
 export default function Signup() {
   // const handleSignup = () => {}
 
   return (
-    <View style={globalStyles.container}>
-      <Formik
-        initialValues={{
-          email: "",
-          name: "",
-          password: "",
-          passwordConfirmation: "",
-        }}
-        // onSubmit={handleSignup}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-          /* and other goodies */
-        }) => (
-          <View>
-            <Text>Email</Text>
-            <TextInput
-              onChangeText={handleChange("email")}
-              value={values.email}
-            />
-            <Text>Name</Text>
-            <TextInput
-              onChangeText={handleChange("name")}
-              value={values.name}
-            />
-            <Text>Password</Text>
-            <TextInput
-              onChangeText={handleChange("password")}
-              value={values.password}
-            />
-            <Text>Confirm Password</Text>
-            <TextInput
-              onChangeText={handleChange("passwordConfirmation")}
-              value={values.passwordConfirmation}
-            />
-          </View>
-        )}
-      </Formik>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={globalStyles.container}>
+        <Formik
+          initialValues={{
+            email: "",
+            name: "",
+            password: "",
+            passwordConfirmation: "",
+          }}
+          // onSubmit={handleSignup}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            /* and other goodies */
+          }) => (
+            <View>
+              <Text>Email</Text>
+              <TextInput
+                onChangeText={handleChange("email")}
+                onBlur={handleBlur("email")}
+                value={values.email}
+              />
+              <Text>Name</Text>
+              <TextInput
+                onChangeText={handleChange("name")}
+                onBlur={handleBlur("name")}
+                value={values.name}
+              />
+              <Text>Password</Text>
+              <TextInput
+                onChangeText={handleChange("password")}
+                onBlur={handleBlur("password")}
+                value={values.password}
+              />
+              <Text>Confirm Password</Text>
+              <TextInput
+                onChangeText={handleChange("passwordConfirmation")}
+                onBlur={handleBlur("passwordConfirmation")}
+                value={values.passwordConfirmation}
+              />
+            </View>
+          )}
+        </Formik>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
