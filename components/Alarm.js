e4a562d503a4aa5484c5c03b73fd995dc745661a
Daniel <danchan020@gmail.com>
2023-01-15 18:16:20 -0800

    home screen with tabs

---
+++ components/Alarm.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Alarm() {
+  return (
+    <View>
+      <Text>Alarm</Text>
+    </View>
+  );
+}
--- components/Home.js
+++ components/Home.js
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Alarm from "./Alarm";
 import SetAlarm from "./SetAlarm";
 import Settings from "./Settings";
 
@@ -7,7 +8,7 @@ const Tab = createBottomTabNavigator();
 export default function Home() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Alarm" component={Home} />
+      <Tab.Screen name="Alarm" component={Alarm} />
       <Tab.Screen name="Set Alarm" component={SetAlarm} />
       <Tab.Screen name="Settings" component={Settings} />
     </Tab.Navigator>
