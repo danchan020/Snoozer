d39a0a0086752357ceb756f313c7311a9b09d93d
Daniel <danchan020@gmail.com>
2023-01-18 20:40:31 -0800

    interval

--- components/Alarm.js
+++ components/Alarm.js
@@ -1,7 +1,21 @@
+import { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import { globalStyles } from "../styles/global";
 
 export default function Alarm() {
+  const [second, setSecond] = useState("");
+  const [minute, setMinute] = useState("");
+  const [hour, setHour] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setHour(new Date().getHours().toString().padStart(2, 0));
+      setMinute(new Date().getHours().toString().padStart(2, 0));
+      setSecond(new Date().getHours().toString().padStart(2, 0));
+    }, 1000);
+    // return () => {clearInterval(interval)};
+  }, [second]);
+
   return (
     <View style={globalStyles.container}>
       <Text>Alarm</Text>
