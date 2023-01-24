2c571c0ff27f19aacfeeca854d380a85ecbfbb67
Daniel <danchan020@gmail.com>
2023-01-23 22:03:58 -0800

    analog clock repo code

--- components/AnalogClock.js
+++ components/AnalogClock.js
@@ -37,9 +37,108 @@ export default class AnalogClock extends Component {
       showSeconds,
     } = this.state;
 
+    let date = new Date();
+
+    if (!hour && hour !== 0) hour = date.getHours();
+    hour = hour > 12 ? hour - 12 : hour;
+
+    if (!minutes && minutes !== 0) minutes = date.getMinutes();
+    minutes = minutes / 5;
+
+    if (!seconds && seconds !== 0) seconds = date.getSeconds();
+    seconds = seconds > 60 ? seconds - 60 : seconds;
+
+    var lanHour = size / 6;
+    var lanMinutes = size / 3.75;
+    var lanSeconds = size / 3.75;
+
     return (
-      <View>
-        <Text>AnalogClock</Text>
+      <View
+        style={{
+          backgroundColor: colorClock,
+          borderRadius: size / 2,
+          justifyContent: "center",
+          alignItems: "center",
+          height: size,
+          width: size,
+        }}
+      >
+        {[...Array(12).keys()].map((i) => {
+          let a = -60 + 30 * i;
+          let b = 60 - 30 * i;
+          return (
+            <View
+              style={{
+                position: "absolute",
+                transform: [
+                  { rotate: a + "deg" },
+                  { translateX: size / 2 - 15 },
+                ],
+              }}
+            >
+              <Text
+                style={{
+                  color: colorNumber,
+                  fontSize: size / 9,
+                  fontWeight: "bold",
+                  transform: [{ rotate: b + "deg" }],
+                }}
+              >
+                {i + 1}
+              </Text>
+            </View>
+          );
+        })}
+        <View
+          style={{
+            zIndex: 1,
+            width: 8,
+            height: 8,
+            borderRadius: 4,
+            backgroundColor: colorCenter,
+          }}
+        />
+        <View
+          style={{
+            position: "absolute",
+            width: lanHour,
+            height: 4,
+            borderRadius: 4,
+            backgroundColor: colorHour,
+            transform: [
+              { rotate: -90 + (hour + minutes / 12) * 30 + "deg" },
+              { translateX: lanHour / 2 },
+            ],
+          }}
+        />
+        <View
+          style={{
+            position: "absolute",
+            width: lanMinutes,
+            height: 4,
+            borderRadius: 4,
+            backgroundColor: colorMinutes,
+            transform: [
+              { rotate: -90 + minutes * 30 + "deg" },
+              { translateX: lanMinutes / 2 },
+            ],
+          }}
+        />
+        {showSeconds && (
+          <View
+            style={{
+              position: "absolute",
+              width: lanSeconds,
+              height: 2,
+              borderRadius: 4,
+              backgroundColor: colorSeconds,
+              transform: [
+                { rotate: -90 + seconds * 6 + "deg" },
+                { translateX: lanSeconds / 2 },
+              ],
+            }}
+          />
+        )}
       </View>
     );
   }
