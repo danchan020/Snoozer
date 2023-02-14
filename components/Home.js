3363f316892c01ed6a58f2ee497a76da2353e410
Daniel <danchan020@gmail.com>
2023-02-14 13:19:56 -0800

    fix render with load state

--- components/Home.js
+++ components/Home.js
@@ -8,7 +8,8 @@ import { useEffect, useState } from "react";
 export default function Home({ user, setUser, setNotificationTitle }) {
   const Tab = createBottomTabNavigator();
   const [refresh, setRefresh] = useState(false);
-  const [userAlarm, setUserAlarm] = useState([]);
+  const [userAlarm, setUserAlarm] = useState({});
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     // async function getUserData() {
@@ -22,25 +23,32 @@ export default function Home({ user, setUser, setNotificationTitle }) {
       const response = await fetch("http://localhost:3000/alarms");
       const alarmJSON = await response.json();
       await setUserAlarm(alarmJSON.find((alarm) => alarm.user_id === user.id));
+      setLoaded(true);
     }
     getAlarmData();
-
     // https://www.geeksforgeeks.org/how-to-calculate-the-number-of-days-between-two-dates-in-javascript/
-    // https://www.w3resource.com/javascript-exercises/javascript-date-exercise-45.php (hours)
-    // https://www.w3resource.com/javascript-exercises/javascript-date-exercise-44.php (minutes)
+    // https://www.w3resource.com/javascript-exercises/javascript-date-exercise-45.php (hours) substring( 0 , 2 )
+    // https://www.w3resource.com/javascript-exercises/javascript-date-exercise-44.php (minutes) substring( 3 , 5 )
 
     // first we need some math to calculate the start of the alarm to the end of the alarm
     // this way we can calculate how many days it will take to reach the goal (end of the alarm) based on the alarm increment
 
+    if (loaded) {
+      console.log(userAlarm.alarm_start.substring(0, 2));
+      console.log(userAlarm.alarm_end.substring(0, 2));
+      console.log(userAlarm.alarm_start.substring(3, 5));
+      console.log(userAlarm.alarm_end.substring(3, 5));
+    }
+
     // create an alarm array, should include the day and time of the user's first alarm
 
-    // want to create the alarm for today
+    // want to create the alarm trigger for today
 
-    // want to create the alarm for the next day
+    // want to create the alarm trigger for the next day
     const tomorrow = new Date(userAlarm.updated_at);
     tomorrow.setDate(tomorrow.getDate() + 1);
-    console.log(tomorrow);
-  }, [refresh]);
+    // console.log(tomorrow);
+  }, [refresh, loaded]);
 
   // console.log(userAlarm.updated_at);
 
